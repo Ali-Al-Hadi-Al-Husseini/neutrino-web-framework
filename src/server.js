3eaf5d8a4de3396cac365aceaf3ca6f567743cf4
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2022-12-23 23:02:16 +0200

    added a custom server and tests

---
+++ src/server.js
@@ -0,0 +1,49 @@
+const Neutrino_pack = require('./neutrino')
+
+const Router = Neutrino_pack.Router
+const Neutrino = Neutrino_pack.Neutrino
+
+
+let app = new Neutrino(5500);
+
+ function setup_Server(){
+
+    app.addroute("/<lilo>", (re, res, dynamicpar) => {
+        res.write("<h1>ALi is  here " + dynamicpar["lilo"] + ' </h1>');
+    });
+    app.addroute("/ali",  (req, res )=> {
+        res.write("<h1>ALi is  here" + "alllllllll" + ' </h1>');
+    });
+    // app.addroute("/ali",  (req, res )=> {
+    //     res.write("<h1>ALi is  here" + "allllllll" + ' </h1>');
+    // });
+    app.addroute("/ali/<lilo>",  (req, res,dynamic )=> {
+        res.write("<h1>ALi is  here " + dynamic["lilo"] + ' </h1>');
+    });
+    app.addroute("/ali/<lilo>/ali",  (req, res,dynamic )=> {
+        res.write("<h1>ALi is  here " + dynamic["lilo"] + " ali" + ' </h1>');
+    });
+    app.addroute("/me",  (req, res,dynamic )=> {
+        res.write("||||||||||||||||||||||||||||||");
+    });
+    app.addroute("/me/<name>",  (req, res,dynamic )=> {
+        res.write("||||||||||||||" +dynamic['name'] + "||||||||||||||||");
+    });
+
+    let router = new Router(app,'/there',(req, res, dynamicpar) => {
+            res.write("<h1> there </h1>");
+        })
+
+    // "<h1>ALi is  here " + dynamic["lilo"] + " " +dynamic['mimo'] + " "+ dynamic["pat"] + ' </h1>'
+    router.addRoute("/<place>",(req, res,dynamic ) => {
+            res.write("<h1>there " + dynamic['place'] + "/h1>");
+        })
+
+
+    // app.use((req, res,next)=> {
+    //     console.log("===================    middleware acssecd here hola hola hola hello    =====================")
+    // })
+     app.start()
+}
+
+setup_Server()
---
+++ src/test_server.py
@@ -0,0 +1,29 @@
+from requests import get
+import unittest 
+
+
+class testServer(unittest.TestCase):
+
+    def test_get(self):
+        test_cases = [
+                    ('/','<h1>Neutrino</h1>'),
+                    ('/me','||||||||||||||||||||||||||||||'),
+                    ('/me/john',"||||||||||||||" + "john"+ "||||||||||||||||"),
+                    ('/hola',"<h1>ALi is  here " + 'hola' + ' </h1>'),
+                    ('/ali',"<h1>ALi is  here" + "alllllllll" + ' </h1>'),
+                    ('/ali/hello',"<h1>ALi is  here " + 'hello' + ' </h1>'),
+                    ('/ali/hello/ali',"<h1>ALi is  here " + 'hello ' + "ali" + ' </h1>'),
+                    ('/there/newyork',"<h1>there newyork/h1>"),
+                    ('/there',"<h1> there </h1>")
+
+
+        ]
+
+        for route,expected_respone in test_cases:
+            response = get('http://127.0.0.1:5500' + route)
+            
+            self.assertEqual(response.text,expected_respone)
+
+
+if __name__ == "__main__":
+    unittest.main()
