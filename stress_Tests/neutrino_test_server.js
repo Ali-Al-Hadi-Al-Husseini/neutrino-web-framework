a4bbfb12ce2c09db093d346d0bcaead991e6bc69
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2023-01-18 14:59:59 +0200

    changed  syntax to new

--- stress_Tests/neutrino_test_server.js
+++ stress_Tests/neutrino_test_server.js
@@ -5,31 +5,31 @@ const Neutrino = Neutrino_pack.Neutrino
 
 let app = new Neutrino(5500);
 
-app.addroute("/<lilo>",async  (req, res) => {
+app.addRoute("/<lilo>",async  (req, res) => {
     await res.sendHtml("<h1>ALi is  here " + req.dynamicParts['lilo'] + ' </h1>');
 
 });
-app.addroute("/ali", async   (req, res )=> {
+app.addRoute("/ali", async   (req, res )=> {
     await res.sendHtml("<h1>ALi is  here" + "alllllllll" + ' </h1>');
 });
 
-app.addroute("/ali/<lilo>", async   (req, res )=> {
+app.addRoute("/ali/<lilo>", async   (req, res )=> {
     await res.sendHtml("<h1>ALi is  here " + req.dynamicParts["lilo"] + ' </h1>');
 });
-app.addroute("/ali/<lilo>/ali",async    (req, res )=> {
+app.addRoute("/ali/<lilo>/ali",async    (req, res )=> {
     await res.sendHtml("<h1>ALi is  here " + req.dynamicParts["lilo"] + " ali" + ' </h1>');
 });
-app.addroute("/me", async   (req, res )=> {
+app.addRoute("/me", async   (req, res )=> {
     await res.sendHtml("||||||||||||||||||||||||||||||");
 
 });
-app.addroute("/me/<name>", async   (req, res )=> {
+app.addRoute("/me/<name>", async   (req, res )=> {
     await res.sendHtml("||||||||||||||" +req.dynamicParts['name'] + "||||||||||||||||");
 });
-app.addroute("/me/ali",  async  (req, res )=> {
+app.addRoute("/me/ali",  async  (req, res )=> {
     await res.redirect('/ali');
 });
-app.addroute('/halo', async  (req,res)=>{
+app.addRoute('/halo', async  (req,res)=>{
     await res.render('index.html')
 })
 
