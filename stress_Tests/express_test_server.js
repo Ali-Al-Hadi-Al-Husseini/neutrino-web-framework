35cb7f6eb2f1b430f7233d2eef8996781bc60178
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2023-01-26 17:33:46 +0200

    removed async since it has no effect

--- stress_Tests/express_test_server.js
+++ stress_Tests/express_test_server.js
@@ -3,39 +3,39 @@ const express = require('express');
 const app = express();
 
 app.ge
-app.get('/', async (req, res) => {
-    await res.send(`<h1>ALi is here </h1>`);
+app.get('/',  (req, res) => {
+     res.send(`<h1>ALi is here </h1>`);
   })
-app.get('/:lilo', async  (req, res) => {
- await  res.send(`<h1>ALi is here ${req.params.lilo}</h1>`);
+app.get('/:lilo',   (req, res) => {
+   res.send(`<h1>ALi is here ${req.params.lilo}</h1>`);
 });
 
-app.get('/ali',  async (req, res) => {
-  await res.send('<h1>ALi is here allllllllll</h1>');
+app.get('/ali',   (req, res) => {
+   res.send('<h1>ALi is here allllllllll</h1>');
 });
 
-app.post('/ali', async  (req, res) => {
-  await res.send('<h1>ALi is here 111111</h1>');
+app.post('/ali',   (req, res) => {
+   res.send('<h1>ALi is here 111111</h1>');
 });
 
-app.get('/ali/:lilo', async  (req, res) => {
-  await res.send(`<h1>ALi is here ${req.params.lilo}</h1>`);
+app.get('/ali/:lilo',   (req, res) => {
+   res.send(`<h1>ALi is here ${req.params.lilo}</h1>`);
 });
 
-app.get('/ali/:lilo/ali', async  (req, res) => {
- await  res.send(`<h1>ALi is here ${req.params.lilo} ali</h1>`);
+app.get('/ali/:lilo/ali',   (req, res) => {
+   res.send(`<h1>ALi is here ${req.params.lilo} ali</h1>`);
 });
 
-app.get('/me',  async (req, res) => {
- await  res.send('||||||||||||||||||||||||||||||');
+app.get('/me',   (req, res) => {
+   res.send('||||||||||||||||||||||||||||||');
 });
 
-app.get('/me/:name',  async (req, res) => {
-  await res.send(`||||||||||||||${req.params.name}||||||||||||||||`);
+app.get('/me/:name',   (req, res) => {
+   res.send(`||||||||||||||${req.params.name}||||||||||||||||`);
 });
 
-app.get('/me/ali',  async (req, res) => {
-  await res.redirect('/ali');
+app.get('/me/ali',   (req, res) => {
+   res.redirect('/ali');
 });
 
 
