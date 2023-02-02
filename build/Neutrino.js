1d8eecb1dd2bffb049b4788c8a081ab0ecd782e2
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2023-02-02 21:13:42 +0200

    chnaged order of cors and removed comment

--- build/Neutrino.js
+++ build/Neutrino.js
@@ -125,9 +125,9 @@ async function readFile(path) {
 function corsMiddleware(allowedDomains) {
     const innerMethod = (req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', allowedDomains);
-        res.setHeader('Access-Control-Allow-Methods', `${Object.keys(req.routeObj.methodsFuncs).join(", ")}`); // should be changed
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         res.setHeader('Access-Control-Max-Age', '3600');
+        res.setHeader('Access-Control-Allow-Methods', `${Object.keys(req.routeObj.methodsFuncs).join(", ")}`);
         next();
     };
     return innerMethod;
@@ -824,7 +824,7 @@ class Neutrino {
         this.use(helmet.xssFilter());
         this.use((req, res, next) => {
             res.setHeader('X-XSS-Protection', '1; mode=block');
-            next()
+            next();
         });
     }
     setAllowedDomains(allowedDomains) {
